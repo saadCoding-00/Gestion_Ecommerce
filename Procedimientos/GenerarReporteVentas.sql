09a4b7031058e7b24a46af961059b8f36e7144a6
saadCoding-00 <saadfaham04@gmail.com>
2025-05-30 09:47:32 +0200

    pequeña midificacion

--- Procedimientos/GenerarReporteVentas.sql
+++ Procedimientos/GenerarReporteVentas.sql
@@ -15,7 +15,6 @@ FROM pedidos p
 INNER JOIN detalles_pedido dp ON p.id = dp.pedido_id
 INNER JOIN productos pr ON pr.id = dp.producto_id
 WHERE p.fecha_pedido BETWEEN p_fecha_inicio AND p_fecha_fin
-AND p.estado = 'completado' -- opcional: solo pedidos finalizados
 GROUP BY dp.producto_id, pr.nombre
 ORDER BY total_ingresos DESC;
 END $$
